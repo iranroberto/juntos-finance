8d6f9107f7925e95362509734f367ca550a8aa73
Iran Astuto <iranx@users.noreply.github.com>
2026-07-29 01:21:21 -0300

    feat: aprimora dashboard e tema

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" data-theme="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
